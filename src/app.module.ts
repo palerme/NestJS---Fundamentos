5b2ab7e8536e3cf56a2ec5538983dcdd520e733f
Joao Guilherme <joaoguilhermefagundes@gmail.com>
2024-01-25 21:20:15 -0300

    appModule: add ConfiguModule, ThrottleModule, creating APP_GUARD

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot({
+      ttl: 60,
+      limit: 100
+    }),
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule)
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService, {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard
+    }
+  ],
 })
 export class AppModule {}
